9efb097ef89d469c41eb45e26d09b77c5b1c7fc7
IvKos81 <talkaf1@yandex.ru>
2021-02-01 21:50:00 +0000

    Добавлены функции в файл main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+/* Функция, возвращающая случайное целое число из переданного диапазона. */
+
+function getRandomNumber(num1, num2) {
+  num1 = Math.ceil(num1);
+  num2 = Math.floor(num2);
+
+  if (num1>=0 && num2>0) {
+    if ((num1 === num2) || (num1 > num2)) {
+      return 'Диапазон задан неверно'
+    } else {
+      return Math.floor(Math.random() * (num2 - num1)) + num1;
+    }
+  } else {
+    return 'Диапазон содержит отрицательное значение'
+  }
+}
+
+getRandomNumber()
+
+/* Функция, возвращающая случайное число с плавающей точкой из переданного диапазона. */
+
+function getRandomCoordinate(num1, num2, x) {
+  if (num1>=0 && num2>0) {
+    if ((num1 === num2) || (num1 > num2)) {
+      return 'Диапазон задан неверно'
+    } else {
+      return ((Math.random() * (num2 - num1)) + num1).toFixed(x);
+    }
+  } else {
+    return 'Диапазон содержит отрицательное значение'
+  }
+}
+getRandomCoordinate()
