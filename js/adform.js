d9ef8f0107eabb06c9c19859bf23a3e533affbd3
IvKos81 <talkaf1@yandex.ru>
2021-03-02 22:28:19 +0000

    Добавлен код работы с кнопкой отправки формы

--- js/adform.js
+++ js/adform.js
@@ -1,1 +1,16 @@
 // работа с данными формы
+
+// отмена отправки формы по умолчанию и отправка методом Fetch
+
+import {sendData} from './server.js'
+import {showGoodSendMessage, showBadSendMessage} from './util.js'
+
+const adForm = document.querySelector('.ad-form');
+
+adForm.addEventListener('submit', function(evt) {
+  evt.preventDefault()
+
+  const formData = new FormData(evt.target);
+
+  sendData (showGoodSendMessage, showBadSendMessage, formData);
+});
