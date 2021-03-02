8cd61d934a973f74419bb6d43a0808e799c4bbf8
IvKos81 <talkaf1@yandex.ru>
2021-03-02 22:25:58 +0000

    Переписана функция отрисовки карты

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import {similarAdverts, createAdvertElement} from './popup.js'
+import {createAdvertElement} from './popup.js'
 import {setInActive, setActive} from './util.js'
 
 setInActive()
@@ -7,71 +7,76 @@ setInActive()
 // создание и отрисовка карты с помощью Leaflet
 
 const ADDRESS = document.querySelector('#address');
+ADDRESS.setAttribute('disabled','disabled');
 const LAT = 35.681700;
 const LNG = 139.75388;
 
-const MAP = L.map('map-canvas').on('load', () => {
-  ADDRESS.value = `${LAT}, ${LNG}`;
-  setActive();
-}).setView({
-  lat: LAT,
-  lng: LNG,
-},12);
+function createMap (array) {
+  const MAP = L.map('map-canvas').on('load', () => {
+    ADDRESS.value = `${LAT}, ${LNG}`;
+    setActive();
+  }).setView({
+    lat: LAT,
+    lng: LNG,
+  },12);
 
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(MAP);
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(MAP);
 
+  // создание главного маркера
 
-// создание главного маркера
-
-
-const MAIN_PIN_ICON = L.icon({
-  iconUrl:'img/main-pin.svg',
-  iconSize:[52,52],
-  iconAnchor: [26,52],
-});
-
-const MAIN_PIN_MARKER = L.marker({
-  lat: LAT,
-  lng: LNG,
-},
-{
-  draggable: true,
-  icon: MAIN_PIN_ICON,
-},
-);
-
-MAIN_PIN_MARKER.addTo(MAP).bindPopup('Координаты: '+ADDRESS.value)
-
-MAIN_PIN_MARKER.on('move',(evt) => {
-  let latlng = evt.target.getLatLng();
-  let lat = latlng.lat.toFixed(5);
-  let lng = latlng.lng.toFixed(5);
-  ADDRESS.value = `${lat}, ${lng}`;
-})
-
-// генерация маркеров для случайных объявлений
-
-similarAdverts.forEach((adv) => {
-
-  // eslint-disable-next-line no-undef
-  const MARKER_PIN_ICON = L.icon({
-    iconUrl:'img/pin.svg',
-    iconSize:[40,40],
-    iconAnchor:[20,40],
+  const MAIN_PIN_ICON = L.icon({
+    iconUrl:'img/main-pin.svg',
+    iconSize:[52,52],
+    iconAnchor: [26,52],
   });
 
-  const MARKER = L.marker({
-    lat: adv.location.lat,
-    lng: adv.location.lng,
+  const MAIN_PIN_MARKER = L.marker({
+    lat: LAT,
+    lng: LNG,
   },
   {
-    icon: MARKER_PIN_ICON,
+    draggable: true,
+    icon: MAIN_PIN_ICON,
   },
   );
-  MARKER.addTo(MAP).bindPopup(createAdvertElement(adv),{keepInView: true});
-});
+
+  MAIN_PIN_MARKER.addTo(MAP).bindPopup('Координаты: '+ADDRESS.value)
+
+  MAIN_PIN_MARKER.on('move',(evt) => {
+    let latlng = evt.target.getLatLng();
+    let lat = latlng.lat.toFixed(5);
+    let lng = latlng.lng.toFixed(5);
+    ADDRESS.value = `${lat}, ${lng}`;
+  })
+
+  // генерация маркеров для случайных объявлений
+
+  array.forEach((adv) => {
+
+    // eslint-disable-next-line no-undef
+    const MARKER_PIN_ICON = L.icon({
+      iconUrl:'img/pin.svg',
+      iconSize:[40,40],
+      iconAnchor:[20,40],
+    });
+
+    const MARKER = L.marker({
+      lat: adv.location.lat,
+      lng: adv.location.lng,
+    },
+    {
+      icon: MARKER_PIN_ICON,
+    },
+    );
+    MARKER.addTo(MAP).bindPopup(createAdvertElement(adv),{keepInView: true});
+  });
+
+  return MAP
+}
+
+export {createMap}
