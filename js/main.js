bbdda70ce075e02fdd210526ef72894681fb4c5d
IvKos81 <talkaf1@yandex.ru>
2021-03-02 22:29:07 +0000

    Добавлен код в точку входа

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 import './data.js'
 import './popup.js'
 import './map.js'
+import './server.js'
+//import './adform.js'
+
+import {getData} from './server.js'
+import {createMap} from './map.js'
+import {showBadReceiveMessage} from './util.js'
+
+
+getData(createMap, showBadReceiveMessage)
