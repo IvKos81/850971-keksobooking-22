a1e6242c9e4e7ad6f90a1ce981ccedba92845017
IvKos81 <talkaf1@yandex.ru>
2021-03-02 22:24:53 +0000

    Созданы функции получения и отправки данных с сервера

---
+++ js/server.js
@@ -0,0 +1,30 @@
+//Запрос данных с сервера
+
+function getData(onSuccess, onFail) {
+  fetch('https://22.javascript.pages.academy/keksobooking/data')
+    .then((response) => response.json())
+    .then((data) => onSuccess(data))
+    .catch(() => {
+      onSuccess();
+      onFail();
+    });
+}
+
+// Отправка данных на сервер
+
+function sendData(onSuccess, onFail, body) {
+  fetch('https://22.javascript.pages.academy/keksobooking', {
+    method: 'POST',
+    body,
+  })
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail();
+      }
+    })
+    .catch(() => onFail());
+}
+
+export {getData, sendData}
