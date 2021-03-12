ee54ae26f646da1860d69a6312b6b867ad9e8d3c
IvKos81 <talkaf1@yandex.ru>
2021-03-12 19:56:06 +0000

    Сделал задание Перламутровые пуговицы - часть 1

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ import './popup.js'
 import './map.js'
 import './server.js'
 import './adform.js'
+import './filter.js'
 
 import {getData} from './server.js'
-import {createMap} from './map.js'
+import {renderMarkers} from './map.js'
 import {showBadReceiveMessage} from './util.js'
+import {changeFilter} from './filter.js'
 
 
-getData(createMap, showBadReceiveMessage)
+// запуск загрузки данных с сервера, внутри функции происходит отрисовка маркеров на карте и перерисовка их в соответствии со значениями фильтров
+
+getData(function(array) {
+  renderMarkers(array);
+  changeFilter(function() {renderMarkers(array)})},showBadReceiveMessage)
+
